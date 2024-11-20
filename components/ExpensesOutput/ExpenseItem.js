33796a008e5c0343b93bfe2bfbc2645265cff7d6
brandoncyu <Brandoncyu@gmail.com>
2024-11-20 15:08:58 -0800

    added user input

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,6 +1,6 @@
 import { Pressable, View, Text, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
-import { getFormatteDate } from "../../util/date";
+import { getFormattedDate } from "../../util/date";
 import { useNavigation } from "@react-navigation/native";
 
 function ExpenseItem ({ id, description, date, amount}) {
@@ -20,7 +20,7 @@ function ExpenseItem ({ id, description, date, amount}) {
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                    <Text style={styles.textBase}>{getFormatteDate(date)}</Text>
+                    <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
                 </View>
                 <View style={styles.amountContainer}>
                     <Text style={styles.amount}>${amount.toFixed(2)}</Text>
